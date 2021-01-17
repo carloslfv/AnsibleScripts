dc2e8d5805b5a9cace223df92a2badaf66763c2a
carloslfv <75767058+carloslfv@users.noreply.github.com>
2021-01-16 23:39:26 -0600

    FirstTry

--- BodyTemplates/EmailScript2.sh
+++ BodyTemplates/EmailScript2.sh
@@ -1,3 +1,5 @@
 #/bin/bash
 cp /BodyTemplates/CheckUpdatesBodyTemplate.html /tmp/"$1.html"
 sed -i 's,{{ Link }},'$2',' "/tmp/$1.html"
+VarDetail=$(cat /tmp/$1/$1.body)
+sed -i 's,{{ Details }},'$VarDetail',' "/tmp/$1.html"
