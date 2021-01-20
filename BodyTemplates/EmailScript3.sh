69a42bb4a0de6b589ad893e8153202f27aa040e7
carloslfv <75767058+carloslfv@users.noreply.github.com>
2021-01-20 17:37:33 -0600

    Change2

--- BodyTemplates/EmailScript3.sh
+++ BodyTemplates/EmailScript3.sh
@@ -2,7 +2,7 @@
 #Attachment and Details on Body
 #BodyTemplate
 python3 /BodyTemplates/JsonPrismInterpreter.py $1 $3 $4 $5 $6
-cp /BodyTemplates/NTNXDSReport.html "/tmp/$1/$1.html"
+#cp /BodyTemplates/NTNXDSReport.html "/tmp/$1/$1.html"
 sed -i 's,{{ Link }},'$2',' "/tmp/$1/$1.html"
 sed -i 's,{{ DetailsTable }},'"`cat /tmp/$1/File.report`"',g' "/tmp/$1/$1.html"
 #storage.capacity_bytes,storage.usage_bytes,storage.reserved_capacity_bytes,storage.free_bytes,data_reduction.compression.user_saved_bytes
