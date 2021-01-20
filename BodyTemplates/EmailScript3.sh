4cdc57bc64d72e8378db36af5f2182b769e19ea3
carloslfv <75767058+carloslfv@users.noreply.github.com>
2021-01-20 17:24:40 -0600

    ok

--- BodyTemplates/EmailScript3.sh
+++ BodyTemplates/EmailScript3.sh
@@ -1,7 +1,6 @@
 #/bin/bash
 #Attachment and Details on Body
 #BodyTemplate
-mkdir /tmp/$1
 python3 /BodyTemplates/JsonPrismInterpreter.py $1 $3 $4 $5 $6
 cp /BodyTemplates/NTNX_DS_Report.html /tmp/$1/$1.html
 sed -i 's,{{ Link }},'$2',' "/tmp/$1/$1.html"
